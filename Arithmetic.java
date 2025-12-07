0cfd61ea4b69e91b50714d82c163119a424ed14e
shunshaan <148305764+Shawnclark02@users.noreply.github.com>
2025-12-07 15:49:15 +0000

    output done

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+class Arithmetic {
+    public static void main(String[] args) {
+        
+        int result = 1 + 15;
+        System.out.println(result);
+
+        result = result - 6;
+        System.out.println(result);
+
+        result = result * 5;
+        System.out.println(result);
+
+        result = result / 3;
+        System.out.println(result);
+
+        result = result + 10;
+        result = result - 15;
+        result = result % 9;
+        System.out.println(result);
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello, kamusta ka kupal");
+        System.out.println("Kamusta, Mundo!");
     }
 }
